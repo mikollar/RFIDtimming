2fcb2d0be2fa875fbf421fcd6284f504738f7c2a
mikollar <mirovemail@gmail.com>
2016-10-27 13:07:55 +0200

    form fixed

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -824,7 +824,7 @@
             this.groupBox3.TabIndex = 37;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "RFID";
-            this.groupBox3.Enter += new System.EventHandler(this.groupBox3_Enter);
+           
             // 
             // groupBox4
             // 
